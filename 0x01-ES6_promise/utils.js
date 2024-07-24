a3a86564ffaea5df9f9e807f1fd69879fc3c7a0e
saraa93-bit <saraa93177228@gmail.com>
2024-07-25 01:10:24 +0300

    promise tasks

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,16 @@
+function uploadPhoto() {
+    return {
+      status: 200,
+      body: 'photo-profile-1',
+    };
+  }
+  
+  function createUser() {
+    return {
+      firstName: 'Guillaume',
+      lastName: 'Salva',
+    };
+  }
+  
+  module.exports = { uploadPhoto, createUser };
+  
